4e9ad9f7c77f10ffa0abce0e0fc028d1f413d337
muskanqed <muskanqed@gmail.com>
2024-04-04 12:53:23 +0530

    First Hello World Program in CPP

---
+++ CPP_Programming_Basics/HelloWorld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!!!" << endl
+         << "Hi" << '\n';
+    cout << "Travel World!!!" << '\n';
+    cout << "Travel World!!!" << '\n';
+    // cout << endl;
+    // cout << '\n';
+
+    return 0;
+}
