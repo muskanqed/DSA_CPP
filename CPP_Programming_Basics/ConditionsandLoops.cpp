b74c631352099e89a0199d2d9756db1d25aa9319
muskanqed <muskanqed@gmail.com>
2024-04-07 23:34:24 +0530

    Some examples of 'if' condition in CPP

--- CPP_Programming_Basics/ConditionsandLoops.cpp
+++ CPP_Programming_Basics/ConditionsandLoops.cpp
@@ -1,14 +1,38 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  
-  int num = 7;
+int main()
+{
 
-  if(num%2 == 0) {
-    cout << "Even" << endl;
-  }
-  else {
-    cout << "Odd" << endl;
-  }
+    int num = 7;
+
+    if (num % 2 == 0)
+    {
+        cout << "Even" << endl;
+    }
+    else
+    {
+        cout << "Odd" << endl;
+    }
+
+    // differntiating output
+    cout << endl
+         << "-----------" << endl;
+
+    int num1 = 0;
+
+    if (num1 > 0)
+    {
+        cout << "Positive" << endl;
+    }
+    else if (num1 < 0)
+    {
+        cout << "Negative" << endl;
+    }
+    else
+    {
+        cout << "Zero" << endl;
+    }
+
+    return 0;
 }
