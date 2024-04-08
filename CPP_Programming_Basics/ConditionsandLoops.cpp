c57a6a4e4c6ba0d8e3a37d89df2b560ed761248e
muskanqed <muskanqed@gmail.com>
2024-04-08 23:07:19 +0530

    for loop in CPP

--- CPP_Programming_Basics/ConditionsandLoops.cpp
+++ CPP_Programming_Basics/ConditionsandLoops.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-
+    // if-else Condition
     int num = 7;
 
     if (num % 2 == 0)
@@ -33,6 +33,30 @@ int main()
     {
         cout << "Zero" << endl;
     }
+
+    // next topic
+    cout << endl
+         << "-----------" << endl;
+
+    // for Loop
+    for (int i = 1; i < 10; i = i + 1)
+    {
+        cout << i << endl;
+    }
+
+    // differntiating output
+    cout << endl
+         << "-----------" << endl;
+
+    for (int i = 0; i < 5; i = i + 1)
+    {
+        cout << "A"
+             << " " << endl;
+    }
+
+    // differntiating output
+    cout << endl
+         << "-----------" << endl;
 
     return 0;
 }
