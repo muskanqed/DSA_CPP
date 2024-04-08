acae05c6185eb7b2436004322dc7abd2460ec22c
muskanqed <muskanqed@gmail.com>
2024-04-08 23:17:05 +0530

    Nested for Loop

--- CPP_Programming_Basics/ConditionsandLoops.cpp
+++ CPP_Programming_Basics/ConditionsandLoops.cpp
@@ -58,5 +58,16 @@ int main()
     cout << endl
          << "-----------" << endl;
 
+    for (int i = 0; i < 3; i = i + 1)
+    {
+        cout << endl
+             << "Outer Loop " << i << endl
+             << endl;
+        for (int j = 0; j < 3; j = j + 1)
+        {
+            cout << "Inner Loop " << j << endl;
+        }
+    }
+
     return 0;
 }
