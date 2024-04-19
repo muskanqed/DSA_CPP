f30d9c8fc5f3688ab31b03ea064f116c4c2547a4
muskanqed <muskanqed@gmail.com>
2024-04-19 17:06:46 +0530

    HollowPyramid Pattern

--- CPP_Programming_Basics/Patterns/HollowPyramid.cpp
+++ CPP_Programming_Basics/Patterns/HollowPyramid.cpp
@@ -5,12 +5,10 @@ int main()
 {
     int n;
     cin >> n;
-
     for (int row = 0; row < n; row++)
     {
         int k = 0;
-
-        for (int col = 0; col < ((2 * n) - 1); col++)
+        for (int col = 0; col < (2 * n) - 1; col++)
         {
             if (col < n - row - 1)
             {
@@ -18,7 +16,10 @@ int main()
             }
             else if (k < 2 * row + 1)
             {
-                cout << "*";
+                if (k == 0 || k == 2 * row || row == n - 1)
+                    cout << "*";
+                else
+                    cout << " ";
                 k++;
             }
             else
@@ -26,6 +27,7 @@ int main()
                 cout << " ";
             }
         }
+
         cout << endl;
     }
     return 0;
