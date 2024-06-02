0d2c87050f05f1ed1aab141ba5b077befc61592a
muskanqed <muskanqed@gmail.com>
2024-06-02 11:47:27 +0530

    Modification to the folder

--- CPP_Programming_Basics/PracticeProblems/PatternPractice.cpp
+++ CPP_Programming_Basics/PracticeProblems/PatternPractice.cpp
@@ -1,6 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    
+int main() {
+    int i = 1; // Initialize i outside the loop
+
+    for(;;) {
+        if(i < 5) {
+            cout << "muskan" << endl;
+            i = i + 1;
+        } else {
+            break;
+        }
+    }
+
+    return 0;
 }
