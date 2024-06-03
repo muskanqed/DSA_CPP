e93132eac775709e9b192aa2238a1b9b41941fa5
muskanqed <muskanqed@gmail.com>
2024-06-03 23:12:53 +0530

    Diamond Pattern

--- CPP_Programming_Basics/Patterns/Diamond.cpp
+++ CPP_Programming_Basics/Patterns/Diamond.cpp
@@ -3,23 +3,35 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
+    int num;
+    cin >> num;
+    int n = num / 2;
+
     for (int row = 0; row < n; row++)
     {
-        // spaces
+        for (int col = 0; col < n - (row + 1); col++)
+        {
+            cout << " ";
+        }
+        for (int col = 0; col < row + 1; col++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    for (int row = 0; row < n; row++)
+    {
+
         for (int col = 0; col < row; col++)
         {
             cout << " ";
         }
 
-        // stars
         for (int col = 0; col < n - row; col++)
         {
             cout << "* ";
         }
         cout << endl;
     }
-    
     return 0;
 }
