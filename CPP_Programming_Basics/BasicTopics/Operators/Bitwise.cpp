3fe9cc19df07ea1b79821f8008123165092cfd1f
muskanqed <muskanqed@gmail.com>
2024-06-11 23:44:37 +0530

    Left and Right shift operators

--- CPP_Programming_Basics/BasicTopics/Operators/Bitwise.cpp
+++ CPP_Programming_Basics/BasicTopics/Operators/Bitwise.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main()
 {
+    // Basics of Bitwise Operators
     int a, b;
     cin >> a >> b;
 
@@ -10,6 +11,20 @@ int main()
     cout << (a | b) << endl;
     cout << (~a) << endl;
     cout << (a ^ b) << endl;
+
+    // Left shift
+
+    int c, n;
+    cin >> c >> n;
+
+    cout << (c << n) << endl;
+
+    // Right shift
+
+    int d;
+    cin >> d;
+
+    cout << (d >> n) << endl;
 
     return 0;
 }
