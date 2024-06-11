23267d156215b048e85b251586348fb29fca7037
muskanqed <muskanqed@gmail.com>
2024-06-11 23:58:07 +0530

    Negative case of left and right shift

--- CPP_Programming_Basics/BasicTopics/Operators/Bitwise.cpp
+++ CPP_Programming_Basics/BasicTopics/Operators/Bitwise.cpp
@@ -4,27 +4,32 @@ using namespace std;
 int main()
 {
     // Basics of Bitwise Operators
-    int a, b;
-    cin >> a >> b;
+    // int a, b;
+    // cin >> a >> b;
 
-    cout << (a & b) << endl;
-    cout << (a | b) << endl;
-    cout << (~a) << endl;
-    cout << (a ^ b) << endl;
+    // cout << (a & b) << endl;
+    // cout << (a | b) << endl;
+    // cout << (~a) << endl;
+    // cout << (a ^ b) << endl;
 
-    // Left shift
+    // // Left shift
 
-    int c, n;
-    cin >> c >> n;
+    // int c, n;
+    // cin >> c >> n;
 
-    cout << (c << n) << endl;
+    // cout << (c << n) << endl;
 
-    // Right shift
+    // // Right shift
 
-    int d;
-    cin >> d;
+    // int d;
+    // cin >> d;
 
-    cout << (d >> n) << endl;
+    // cout << (d >> n) << endl;
+
+    // Garbage value
+    int g;
+    cout << (g << -2) << endl; // Garbage value in case of Left Shift
+    cout << (g >> -2);         // 0 in case of Right shift
 
     return 0;
 }
