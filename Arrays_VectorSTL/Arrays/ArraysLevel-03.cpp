dc945ca7515fb6d34a34ee2b1086e878f6effeb6
muskanqed <muskanqed@gmail.com>
2024-06-30 22:16:45 +0530

    2D Array in CPP

--- Arrays_VectorSTL/Arrays/ArraysLevel-03.cpp
+++ Arrays_VectorSTL/Arrays/ArraysLevel-03.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main()
 {
+    // 2D Array
+    // Creating and Initialization a Two Array
+    int a[2][4]{{1, 2, 3, 4}, {5, 6, 7, 8}};
+
+    // Accessing a 2D Array
+    cout << a[0][0] << endl;
 
     return 0;
 }
