e39264a3b152a7652fd7305cb04e2ee41007da93
muskanqed <muskanqed@gmail.com>
2024-07-01 23:53:19 +0530

    Taking Input in 2D array from user

--- Arrays_VectorSTL/Arrays/ArraysLevel-03.cpp
+++ Arrays_VectorSTL/Arrays/ArraysLevel-03.cpp
@@ -28,19 +28,37 @@ void print2DArrayColWise(int arr[][4], int rows, int cols)
     }
 }
 
+void inputFromUser(int arr[][4], int row, int col)
+{
+    cout << "Enter Array Elements" << endl;
+
+    for (int i = 0; i < col; i++)
+    {
+        for (int j = 0; j < row; j++)
+        {
+            cout << "Enter Array Elements" << i << " " << j;
+            cin >> arr[i][j];
+        }
+    }
+}
+
 int main()
 {
     // 2D Array
     // Creating and Initialization a Two Array
-    int a[3][4] = {{1, 2, 3, 4},
-                   {5, 6, 7, 8},
-                   {11, 12, 13, 14}};
+    // int a[3][4] = {{1, 2, 3, 4},
+    //                {5, 6, 7, 8},
+    //                {11, 12, 13, 14}};
     // int a[2][4]{{1, 2, 3, 4}, {5, 6, 7, 8}};
-
     int row = 3;
     int col = 4;
+    int a[3][4];
+
+    inputFromUser(a, row, col);
+
     print2DArray(a, row, col);
-    print2DArrayColWise(a, row, col);
+    // print2DArrayColWise(a, row, col);
+    inputFromUser(a, row, col);
 
     // Accessing a 2D Array
     // cout << a[0][0] << endl;
