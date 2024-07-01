7fad9ed7891c695596000b5ee62164163dccdcae
muskanqed <muskanqed@gmail.com>
2024-07-01 23:07:57 +0530

    Printing 2D array

--- Arrays_VectorSTL/Arrays/ArraysLevel-03.cpp
+++ Arrays_VectorSTL/Arrays/ArraysLevel-03.cpp
@@ -1,14 +1,53 @@
 #include <iostream>
 using namespace std;
 
+void print2DArray(int arr[][4], int rows, int cols)
+{
+    cout << "Printing Array" << endl;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void print2DArrayColWise(int arr[][4], int rows, int cols)
+{
+    cout << "Printing Array" << endl;
+
+    for (int i = 0; i < cols; i++)
+    {
+        for (int j = 0; j < rows; j++)
+        {
+            cout << arr[j][i] << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
     // 2D Array
     // Creating and Initialization a Two Array
-    int a[2][4]{{1, 2, 3, 4}, {5, 6, 7, 8}};
+    int a[3][4] = {{1, 2, 3, 4},
+                   {5, 6, 7, 8},
+                   {11, 12, 13, 14}};
+    // int a[2][4]{{1, 2, 3, 4}, {5, 6, 7, 8}};
+
+    int row = 3;
+    int col = 4;
+    print2DArray(a, row, col);
+    print2DArrayColWise(a, row, col);
 
     // Accessing a 2D Array
-    cout << a[0][0] << endl;
+    // cout << a[0][0] << endl;
+
+    // 2D Array is also stored in the memory using continuous memory allocation
+    // Formula: (NumberofCol * i) + j
+    // Always specify number of col, row not needed like int arr[][4] = {{1,2,3,5},{1,2,3,10}};
 
     return 0;
 }
