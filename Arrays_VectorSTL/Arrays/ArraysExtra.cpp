e855a68ebaf5c1944efbaa606d663f9201ea72e9
muskanqed <muskanqed@gmail.com>
2024-07-03 23:10:04 +0530

    Sort colors problem from leetcode

--- Arrays_VectorSTL/Arrays/ArraysExtra.cpp
+++ Arrays_VectorSTL/Arrays/ArraysExtra.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 void shiftNegativeNumbersLeftSide()
@@ -22,8 +23,32 @@ void shiftNegativeNumbersLeftSide()
     }
 }
 
+// Leetcode Problem
+void sortColors()
+{
+    // In-place - cannot use extra memory
+    // Dutch National Flag - Problem
+    vector<int> a = {0, 0, 1, 0, 2, 0, 0, 0, 2, 1, 1};
+    int n = 11;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (a[j] > a[i])
+            {
+                swap(a[i], a[j]);
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+}
+
 int main()
 {
-    shiftNegativeNumbersLeftSide();
+    // shiftNegativeNumbersLeftSide();
+    sortColors();
     return 0;
 }
