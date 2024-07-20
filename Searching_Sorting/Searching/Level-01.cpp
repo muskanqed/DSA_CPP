17e32fbf8e8ca24363b4f52d6af5717ff2a32ba8
muskanqed <muskanqed@gmail.com>
2024-07-20 23:03:53 +0530

    Basics of Binary Search

--- Searching_Sorting/Searching/Level-01.cpp
+++ Searching_Sorting/Searching/Level-01.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 using namespace std;
 
+int binarySearch()
+{
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = 5;
+    int target = 100;
+    int start = 0;
+    int end = n - 1;
+    int mid = (start + end) / 2;
+    while (start <= end)
+    {
+        if (arr[mid] == target)
+        {
+            return arr[mid];
+        }
+        else if (target > arr[mid])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+        // update mid
+        mid = (start + end) / 2;
+    }
+    return false;
+}
+
 int main()
 {
+    cout << binarySearch();
     return 0;
 }
