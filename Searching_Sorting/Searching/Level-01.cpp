270c809d72b32a31454af2800585c0d3c3ac0232
muskanqed <muskanqed@gmail.com>
2024-07-21 22:55:15 +0530

    Total Occurence of Element

--- Searching_Sorting/Searching/Level-01.cpp
+++ Searching_Sorting/Searching/Level-01.cpp
@@ -31,11 +31,11 @@ int binarySearch()
 
 int findFirstOccurence()
 {
-    int arr[] = {30, 30, 30, 30, 30, 40, 50};
+    int arr[] = {10, 20, 30, 40, 40, 40, 50};
     int n = 7;
     int start = 0;
     int end = n - 1;
-    int target = 30;
+    int target = 40;
     int ans = -1;
 
     while (start <= end)
@@ -61,9 +61,48 @@ int findFirstOccurence()
     return ans;
 }
 
+int findLastOccurence()
+{
+    int arr[] = {10, 20, 30, 40, 40, 40, 50};
+    int n = 7;
+    int s = 0;
+    int e = n - 1;
+    int target = 40;
+    int ans = -1;
+    while (s <= e)
+    {
+        int mid = s + (e - s) / 2;
+
+        if (target == arr[mid])
+        {
+            ans = mid;
+            s = mid + 1;
+        }
+        else if (target < arr[mid])
+        {
+            e = mid - 1;
+        }
+        else if (target > arr[mid])
+        {
+            s = mid + 1;
+        }
+    }
+    return ans;
+}
+
+int findTotalOccurence()
+{
+    int firstOccurence = findFirstOccurence();
+    int lastOccurence = findLastOccurence();
+    int totalOccurence = lastOccurence - firstOccurence + 1;
+    return totalOccurence;
+}
+
 int main()
 {
     // cout << binarySearch();
-    cout << findFirstOccurence();
+    // cout << findFirstOccurence();
+    // cout << findLastOccurence();
+    cout << findTotalOccurence();
     return 0;
 }
