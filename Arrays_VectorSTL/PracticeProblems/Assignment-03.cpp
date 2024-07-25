6a21cce7875ed82a585e8511a3fbbaf25661bcfc
muskanqed <muskanqed@gmail.com>
2024-07-25 23:48:10 +0530

    Rearrange the elements leetcode Problem

--- Arrays_VectorSTL/PracticeProblems/Assignment-03.cpp
+++ Arrays_VectorSTL/PracticeProblems/Assignment-03.cpp
@@ -761,6 +761,27 @@ void rotateArrayUsingReversalMethod()
     }
 }
 
+vector<int> rearrangeArray(vector<int> &nums)
+{
+    vector<int> ans(nums.size(), -1);
+    int odd = 1, even = 0;
+    for (int i = 0; i < nums.size(); ++i)
+    {
+        int num = nums[i];
+        if (num > 0)
+        {
+            ans[even] = num;
+            even += 2;
+        }
+        else
+        {
+            ans[odd] = num;
+            odd += 2;
+        }
+    }
+    return ans;
+}
+
 int main()
 {
     // Two Sum two Approaches
@@ -874,7 +895,10 @@ int main()
     */
 
     // Rotate Array 1D K times
-    rotateArrayUsingReversalMethod();
+    // rotateArrayUsingReversalMethod();
+
+    vector<int> nums = {9, 10, 3, 4, 5};
+    rearrangeArray(nums);
 
     return 0;
 }
