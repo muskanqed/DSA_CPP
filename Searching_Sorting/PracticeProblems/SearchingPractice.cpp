1b1e46734ba6e58df39887e8df58cde881a73a9a
muskanqed <muskanqed@gmail.com>
2024-07-29 23:35:15 +0530

    Some more practice problem

--- Searching_Sorting/PracticeProblems/SearchingPractice.cpp
+++ Searching_Sorting/PracticeProblems/SearchingPractice.cpp
@@ -29,10 +29,41 @@ int findTarget()
             start = mid + 1;
         }
     }
+
+    return -1;
+}
+
+int findFirstOccurence()
+{
+    int arr[] = {10, 20, 30, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 30;
+    int start = 0;
+    int end = n - 1;
+    int ans = 0;
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        if (target == arr[mid])
+        {
+            ans = mid;
+            end = mid - 1;
+        }
+        else if (target > arr[mid])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+    }
+    return ans;
 }
 
 int main()
 {
-    cout << findTarget();
+    // cout << findTarget();
+    cout << findFirstOccurence();
     return 0;
 }
