f7072a8f26a8a5ad6c47587b6b8d92018a178579
muskanqed <muskanqed@gmail.com>
2024-08-06 23:29:44 +0530

    Some more practice problem

--- Searching_Sorting/Searching/Level-02.cpp
+++ Searching_Sorting/Searching/Level-02.cpp
@@ -69,33 +69,7 @@ void search()
 
 int findSquareRoot(int x)
 {
-    int s = 0;
-    int e = x;
-    long long int mid = s + (e - s) / 2;
-    int ans = -1;
 
-    while (s <= e)
-    {
-        // kya mid hi toh answer nahi
-        if (mid * mid == x)
-        {
-            return mid;
-        }
-        else if (mid * mid < x)
-        {
-            // ans store
-            // right me jao
-            ans = mid;
-            s = mid + 1;
-        }
-        else
-        {
-            // left me jana h
-            e = mid - 1;
-        }
-        mid = s + (e - s) / 2;
-    }
-    return ans;
 }
 
 int main()
