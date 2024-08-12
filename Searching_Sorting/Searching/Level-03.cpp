7dc953e8b1618b3d84051fe0c6493378b31da557
muskanqed <muskanqed@gmail.com>
2024-08-12 23:03:18 +0530

    Dividing using binary search for both negative and postive number

--- Searching_Sorting/Searching/Level-03.cpp
+++ Searching_Sorting/Searching/Level-03.cpp
@@ -1,133 +1,43 @@
 #include <iostream>
+
 using namespace std;
 
-int getQuotient(int divisor, int dividend) {
-  int s = 0;
-  int e = dividend;
-  int ans = -1;
-  int mid = s + (e-s)/2;
+int findQuotient(int dividend, int divisor)
+{
+    int s = 0, e = dividend;
+    int ans = -1;
 
-  while(s <= e) {
-    
-    if(mid * divisor == dividend) {
-      return mid;
+    while (s <= e)
+    {
+        long long mid = s + (e - s) / 2; // Use long long to prevent overflow
+        if (mid * divisor == dividend)
+        {
+            return mid;
+        }
+        else if (mid * divisor < dividend)
+        {
+            ans = mid;
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid - 1;
+        }
     }
-    if(mid * divisor < dividend) {
-      //ans store
-      ans = mid;
-      //right me jao
-      s = mid+1;
-    }
-    else {
-      //left
-      e = mid - 1;
-    }
-    mid = s + (e-s)/2;
-  }
-  return ans;
-}
- 
-int searchNearlySorted(int arr[], int n, int target) {
-  int s = 0;
-  int e = n-1;
-  int mid = s + (e-s)/2;
-
-  while(s <= e) {
-    if(mid-1 >= 0 &&  arr[mid-1] == target) {
-      return mid-1;
-    }
-    if(arr[mid] == target)
-      return mid;
-    
-    if(mid+1 < n && arr[mid+1] == target)
-      return mid+1;
-
-    if(target > arr[mid] ){
-      //right
-      s = mid + 2;
-    }
-    else {
-      //left
-      e = mid - 2;
-    }
-    mid = s + (e-s)/2;
-  }
-  return -1;
+    return ans;
 }
 
-int findOddOccuringElement(int arr[], int n) {
-  int s = 0;
-  int e = n-1;
-  int mid = s + (e-s)/2;
-
-  while(s <= e) {
-
-    //single element
-    if(s == e) {
-      return s;
+int main()
+{
+    int dividend = -29;
+    int divisor = -7;
+    int quotient = findQuotient(abs(dividend), abs(divisor));
+    if ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0))
+    {
+        quotient = 0 - quotient;
     }
 
-    //mid check -> even or odd
-    if(mid & 1) { //mid&1 -> true -> odd number
-      if(mid-1 >=0 && arr[mid-1] == arr[mid]) {
-        //right me jao
-        s = mid + 1;
-      }
-      else {
-        //left me jao
-        e = mid - 1;
-      }
-    }
-    else {
-      //even
-      if(mid+1 < n && arr[mid] == arr[mid+1]){
-        //right me jao
-        s = mid + 2;
-      }
-      else {
-        //ya toh main right part pr khada hu
-        //ya toh main answer k upar khada hu
-        //that's why e = mid krra hu
-        // kyoki e = mid - 1; se ans lost ho skta h 
-        e = mid;    
-      }
-    }
-    mid = s + (e-s)/2;
-  }
-  return -1;
-}
+    cout << "Quotient: " << quotient << endl;
 
-
-
-
-
-
-
-
-
-
-
-
-int main() {
-
-
-
-  int arr[] = {10,10,10};
-  int n = 13;
-
-  int ansIndex = findOddOccuringElement(arr, n);
-
-  cout << "Final Ans is: "<< arr[ansIndex] << endl; 
-
-  
-
-
-
-
-
-
-
-
-  
-  return 0;
+    return 0;
 }
