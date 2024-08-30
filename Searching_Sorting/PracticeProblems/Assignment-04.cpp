230aab13d3296090cad27c7a56932c3180c0c124
muskanqed <muskanqed@gmail.com>
2024-08-30 18:21:31 +0530

    Same problem using binary search

--- Searching_Sorting/PracticeProblems/Assignment-04.cpp
+++ Searching_Sorting/PracticeProblems/Assignment-04.cpp
@@ -9,7 +9,7 @@ int findPairs()
 {
     vector<int> v = {1, 1, 1, 1, 1};
     int n = v.size();
-    int k = 0;
+    int k = 2;
     sort(v.begin(), v.end());
     set<pair<int, int>> ans;
     int i = 0;
@@ -39,8 +39,54 @@ int findPairs()
     return ans.size();
 }
 
+int binarySearch(vector<int> &nums, int start, int x)
+{
+    int end = nums.size() - 1;
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        if (nums[mid] == x)
+        {
+            return mid;
+        }
+        else if (x > nums[mid])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int findPairsUsingBinarySearch()
+{
+    vector<int> nums = {1, 1, 1, 1, 1, 1};
+    int n = nums.size();
+    int k = 2;
+    sort(nums.begin(), nums.end());
+    set<pair<int, int>> ans;
+    for (int i = 0; i < n; i++)
+    {
+        if (binarySearch(nums, i + 1, nums[i] + k) != -1)
+        {
+            ans.insert({nums[i], nums[i] + k});
+        }
+    }
+    // for (const auto &p : ans)
+    // {
+    //     cout << "(" << p.first << ", " << p.second << ")" << endl;
+    // }
+    return ans.size();
+}
+
 int main()
 {
-    cout << findPairs();
+    // Time Complexity will be O(nlogn)
+    cout << findPairs() << endl;
+    // Time Complexity will be O(nlogn)
+    cout << findPairsUsingBinarySearch();
     return 0;
 }
